1fd871275a909975eb5b5a7125798fecac521784
github username <github email address>
2019-04-30 10:29:20 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,15 @@
 
 def key_for_min_value(name_hash)
 
+  item = nil
+  value = Float::INFINITY
+
+  name_hash.each do |this_item, this_value|
+    if this_value < value
+      value = this_value
+      item = this_item
+    end 
+  end
+
+  item
 end
